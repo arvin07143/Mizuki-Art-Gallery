1683da898991b58ffc7579c7768c05bab0638626
arvin07143 <arvin07143@gmail.com>
2021-02-24 00:29:50 +0800

    removed testing labels

--- Assignment/App_Pages/ManageArt.aspx.designer.cs
+++ Assignment/App_Pages/ManageArt.aspx.designer.cs
@@ -24,15 +24,6 @@ namespace Assignment
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// lblTest control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblTest;
-
-        /// <summary>
         /// formArtName control.
         /// </summary>
         /// <remarks>
