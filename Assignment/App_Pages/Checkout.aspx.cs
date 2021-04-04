035ace9615c614d266744c9b7839f0684dbb8052
Jeffrey <69193786+jeffreyoh00@users.noreply.github.com>
2021-04-04 15:55:56 +0800

    .

--- Assignment/App_Pages/Checkout.aspx.cs
+++ Assignment/App_Pages/Checkout.aspx.cs
@@ -7,6 +7,7 @@ using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Linq;
 
 namespace Assignment.App_Pages
 {
@@ -47,13 +48,14 @@ namespace Assignment.App_Pages
             SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
 
             con.Open();
-            SqlCommand cmdAddOrder = new SqlCommand("INSERT INTO [dbo].[Order] (Username, Date, Total) VALUES (@Username, @Date, @Total);", con);
+            SqlCommand cmdAddOrder = new SqlCommand("INSERT INTO [dbo].[Order] VALUES (@Username, @Date, @Total, @PaymentType, @CardNumber, @DeliveryAddress, @RecipientName);", con);
             cmdAddOrder.Parameters.AddWithValue("@Username", Session["Username"].ToString());
             cmdAddOrder.Parameters.AddWithValue("@Date", DateTime.Now.ToString("dd-MM-yyyy"));
             cmdAddOrder.Parameters.AddWithValue("@Total", total);
+            cmdAddOrder.Parameters.AddWithValue("@PaymentType", rblPayment.Text);
             cmdAddOrder.Parameters.AddWithValue("@CardNumber", txtCardNumber.Text);
             cmdAddOrder.Parameters.AddWithValue("@DeliveryAddress", txtAddress.Text);
-            cmdAddOrder.Parameters.AddWithValue("@RecipentName", txtRecipentName.Text);
+            cmdAddOrder.Parameters.AddWithValue("@RecipientName", txtRecipentName.Text);
             cmdAddOrder.ExecuteNonQuery();
             con.Close();
 
@@ -80,6 +82,43 @@ namespace Assignment.App_Pages
         }
 
         protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        //kyao de function
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string num = args.Value;
+
+            if (num.Length != 14)
+            {
+                CustomValidator1.ErrorMessage = "Card Number should be 14 digits";
+                args.IsValid = false;
+            }
+
+            if (rblPayment.Text == "Visa")
+            {
+                if (num.First() != '4')
+                {
+                    CustomValidator1.ErrorMessage = "Visa Card start with '4'";
+                    args.IsValid = false;
+                }
+
+            }
+
+            if (rblPayment.Text == "Master")
+            {
+                if (num.First() != '5')
+                {
+                    CustomValidator1.ErrorMessage = "Master Card start with '5'";
+                    args.IsValid = false;
+
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
